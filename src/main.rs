70d6fb5e60b92b5a540ee17d53fd95e370fa7330
yuu <yuuxzy@gmail.com>
2019-07-22 18:15:51 +0900

    use std::future::Future via tokio

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,19 @@
-fn main() {
-    println!("Hello, world!");
+#![feature(async_await)]
+
+use tokio;
+
+async fn foo() -> u8 { 5 }
+
+fn bar() -> impl std::future::Future<Output = u8> {
+    async {
+        let x: u8 = foo().await;
+        println!("{}", x);
+        x + 5
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    let x = bar().await;
+    println!("{}", x);
 }
