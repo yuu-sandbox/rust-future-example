70dca08a0dd9ab82844a9c07ea56e3c4c643db05
yuu <yuuxzy@gmail.com>
2019-07-25 17:43:20 +0900

    unix domain socket read/write

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,30 @@
 #![feature(async_await)]
 
-use tokio;
+use std::error::Error;
 
-async fn foo() -> u8 { 5 }
-
-fn bar() -> impl std::future::Future<Output = u8> {
-    async {
-        let x: u8 = foo().await;
-        println!("{}", x);
-        x + 5
-    }
-}
+use tokio::net::UnixListener;
+use tokio::net::UnixStream;
+use tokio::io::{AsyncReadExt, AsyncWriteExt};
+use futures::future::try_join;
 
 #[tokio::main]
-async fn main() {
-    let x = bar().await;
-    println!("{}", x);
+async fn main() -> Result<(), Box<dyn Error>> {
+    let addr = "/tmp/can_dummy";
+    let mut listener = UnixListener::bind(&addr).unwrap();
+
+    let accept = listener.accept();
+    let connect = UnixStream::connect(&addr);
+
+    let ((mut server, _), mut client) = try_join(accept, connect).await?;
+    let write_len = client.write(b"hello").await?;
+    assert_eq!(write_len, 5);
+    drop(client);
+
+    let mut buf = [0u8; 5];
+    server.read_exact(&mut buf).await?;
+    assert_eq!(&buf, b"hello");
+    let len = server.read(&mut buf).await?;
+    assert_eq!(len, 0);
+
+    Ok(())
 }
