2d83da9ef7f02c920ad6d65b96cb35f789743fe9
yuu <yuuxzy@gmail.com>
2019-07-26 16:55:01 +0900

    wait client by using .await

--- src/main.rs
+++ src/main.rs
@@ -1,30 +1,23 @@
 #![feature(async_await)]
-
-use std::error::Error;
-
-use tokio::net::UnixListener;
-use tokio::net::UnixStream;
+use std::os::unix::net::SocketAddr;
+use tokio_uds::{UnixListener, UnixStream};
 use tokio::io::{AsyncReadExt, AsyncWriteExt};
-use futures::future::try_join;
 
+async fn handler(mut stream: UnixStream, addr: SocketAddr) {
+    eprintln!("connected: {:?}", addr);
+    let mut buf = [0u8; 5];
+    stream.read_exact(&mut buf).await;
+    eprintln!("{:?}", buf);
+}
+
+/// echo -en "OK" |socat stdio /tmp/can_dummy
 #[tokio::main]
-async fn main() -> Result<(), Box<dyn Error>> {
+async fn main() {
     let addr = "/tmp/can_dummy";
     let mut listener = UnixListener::bind(&addr).unwrap();
 
-    let accept = listener.accept();
-    let connect = UnixStream::connect(&addr);
-
-    let ((mut server, _), mut client) = try_join(accept, connect).await?;
-    let write_len = client.write(b"hello").await?;
-    assert_eq!(write_len, 5);
-    drop(client);
-
-    let mut buf = [0u8; 5];
-    server.read_exact(&mut buf).await?;
-    assert_eq!(&buf, b"hello");
-    let len = server.read(&mut buf).await?;
-    assert_eq!(len, 0);
-
-    Ok(())
+    while let Ok(client) = listener.accept().await {
+        let (stream, addr) = client;
+        handler(stream, addr).await;
+    }
 }
